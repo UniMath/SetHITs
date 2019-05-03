db2b1488ebb5d2e29b56f21191bfd4402b31eba7
Niels van der Weide <n.vanderweide@science.ru.nl>
2019-05-04 00:01:35 +0200

    Finished proof that integers form the initial ring

--- code/examples/rings.v
+++ code/examples/rings.v
@@ -263,21 +263,21 @@ Section RingProjections.
     : ∏ (x : ring_carrier), x + (- x) = 0
     := λ x, ring_plus_com x (- x) @ ring_min_plus x.
 
-  Definition ring_mul_assoc
+  Definition ring_mult_assoc
     : ∏ (x₁ x₂ x₃ : ring_carrier), (x₁ * x₂) * x₃ = x₁ * (x₂ * x₃)
     := λ x₁ x₂ x₃, pr2 R m_assoc ((x₁ ,, x₂) ,, x₃).
 
-  Definition ring_one_mul
+  Definition ring_one_mult
     : ∏ (x : ring_carrier), 1 * x = x
     := λ x, pr2 R m_unit x.
 
-  Definition ring_mul_com
+  Definition ring_mult_com
     : ∏ (x y : ring_carrier), x * y = y * x
     := λ x y, pr2 R m_com (x ,, y).
 
-  Definition ring_mul_one
+  Definition ring_mult_one
     : ∏ (x : ring_carrier), x * 1 = x
-    := λ x, ring_mul_com x 1 @ ring_one_mul x.
+    := λ x, ring_mult_com x 1 @ ring_one_mult x.
 
   Definition ring_left_distr
     : ∏ (x₁ x₂ x₃ : ring_carrier), x₁ * (x₂ + x₃) = (x₁ * x₂) + (x₁ * x₃)
@@ -287,9 +287,9 @@ Section RingProjections.
     : ∏ (x₁ x₂ x₃ : ring_carrier), (x₂ + x₃) * x₁ = (x₂ * x₁) + (x₃ * x₁).
   Proof.
     intros x₁ x₂ x₃.
-    rewrite ring_mul_com.
+    rewrite ring_mult_com.
     rewrite ring_left_distr.
-    rewrite !(ring_mul_com _ x₁).
+    rewrite !(ring_mult_com _ x₁).
     reflexivity.
   Qed.
 End RingProjections.
@@ -337,3 +337,228 @@ Proof.
     + exact (λ x, mult_com (pr1 x) (pr2 x)).
     + exact (λ x, distr (pr11 x) (pr21 x) (pr2 x)).
 Defined.
+
+(** Some laws for rings *)
+Section Laws.
+  Context {Ring : ring_cat}.
+
+  Local Notation "'R'" := (alg_carrier Ring).
+  Local Notation "x + y" := (ring_plus Ring x y).
+  Local Notation "x * y" := (ring_mult Ring x y).
+  Local Notation "- x" := (ring_minus Ring x).
+  Local Notation "'0'" := (ring_zero Ring).
+  Local Notation "'1'" := (ring_one Ring).
+
+  Definition ring_cancel_plus_left
+    : ∏ {x y : R} (z : R), z + x = z + y → x = y.
+  Proof.
+    intros x y z H.
+    refine (_ @ maponpaths (λ r, (- z) + r) H @ _).
+    - rewrite <- ring_plus_assoc.
+      rewrite ring_min_plus.
+      rewrite ring_zero_plus.
+      reflexivity.
+    - rewrite <- ring_plus_assoc.
+      rewrite ring_min_plus.
+      rewrite ring_zero_plus.
+      reflexivity.
+  Qed.
+
+  Definition ring_cancel_plus_right
+    : ∏ {x y : R} ( z : R), x + z = y + z → x = y.
+  Proof.
+    intros x y z H.
+    refine (_ @ maponpaths (λ r, r + (- z)) H @ _).
+    - rewrite ring_plus_assoc.
+      rewrite ring_plus_min.
+      rewrite ring_plus_zero.
+      reflexivity.
+    - rewrite ring_plus_assoc.
+      rewrite ring_plus_min.
+      rewrite ring_plus_zero.
+      reflexivity.
+  Qed.      
+
+  Definition ring_minus_eq
+    : ∏ (x y : R), x + y = 0 → x = - y.
+  Proof.
+    intros x y H.
+    apply (ring_cancel_plus_right y).
+    rewrite H.
+    rewrite ring_min_plus.
+    reflexivity.
+  Qed.
+
+  Definition ring_inverse_unique'
+    : ∏ (x : R) {y z : R}, x + y = 0 → x + z = 0 → y = z.
+  Proof.
+    intros x y z H₁ H₂.
+    apply (ring_cancel_plus_left x).
+    exact (H₁ @ !H₂).
+  Qed.
+
+  Definition ring_inverse_unique
+    : ∏ {x y : R}, x + y = 0 → -x = y.
+  Proof.
+    intros x y H.
+    symmetry.
+    apply (ring_inverse_unique' x).
+    - exact H.
+    - apply ring_plus_min.
+  Qed.
+
+  Definition ring_mult_zero
+    : ∏ (x : R), x * 0 = 0.
+  Proof.
+    intro x.
+    apply (ring_cancel_plus_right (x * 0)).
+    rewrite ring_zero_plus.
+    rewrite <- ring_left_distr.
+    rewrite ring_zero_plus.
+    reflexivity.
+  Qed.
+  
+  Definition ring_zero_mult
+    : ∏ (x : R), 0 * x = 0.
+  Proof.
+    intro x.
+    rewrite ring_mult_com.
+    apply ring_mult_zero.
+  Qed.
+  
+  Definition ring_mult_minus
+    : ∏ (x y : R), x * (- y) = -(x * y).
+  Proof.
+    intros x y.
+    apply (ring_inverse_unique' (x * y)).
+    - rewrite <- ring_left_distr.
+      rewrite ring_plus_min.
+      apply ring_mult_zero.
+    - apply ring_plus_min.
+  Qed.
+
+  Definition ring_minus_zero
+    : - 0 = 0.
+  Proof.
+    apply ring_inverse_unique.
+    apply ring_plus_zero.
+  Qed.
+
+  Definition ring_minus_minus
+    : ∏ (x : R), -(- x) = x.
+  Proof.
+    intro x.
+    apply ring_inverse_unique.
+    apply ring_min_plus.
+  Qed.
+
+  Definition ring_inv_plus
+    : ∏ (x y : R), -(x + y) = (-x) + (-y).
+  Proof.
+    intros x y.
+    apply ring_inverse_unique.
+    rewrite (ring_plus_com _ x y).
+    rewrite ring_plus_assoc.
+    refine (_ @ _).
+    {
+      apply maponpaths.
+      rewrite <- ring_plus_assoc.
+      rewrite ring_plus_min.
+      apply ring_zero_plus.
+    }
+    apply ring_plus_min.
+  Qed.    
+End Laws.
+
+(** Builder for ring homomorphisms *)
+Section BuildRingMap.
+  Context {R₁ R₂ : ring_cat}.
+  Variable (f : alg_carrier R₁ → alg_carrier R₂)
+           (f_p : ∏ (x₁ x₂ : alg_carrier R₁),
+                  f (ring_plus R₁ x₁ x₂)
+                  =
+                  ring_plus R₂ (f x₁) (f x₂))
+           (f_m : ∏ (x₁ x₂ : alg_carrier R₁),
+                  f (ring_mult R₁ x₁ x₂)
+                  =
+                  ring_mult R₂ (f x₁) (f x₂))
+           (f_o : f (ring_one R₁) = ring_one R₂).
+
+  Local Definition f_z
+    : f (ring_zero R₁) = ring_zero R₂.
+  Proof.
+    apply (ring_cancel_plus_right (f (ring_zero R₁))).
+    rewrite ring_zero_plus.
+    rewrite <- f_p.
+    exact (maponpaths f (ring_plus_zero _ _)).
+  Qed.
+
+  Local Definition f_minus
+    : ∏ (x : alg_carrier R₁), f (ring_minus R₁ x) = ring_minus R₂ (f x).
+  Proof.
+    intro x.
+    apply ring_minus_eq.
+    rewrite <- f_p.
+    refine (_ @ f_z).
+    apply maponpaths.
+    apply ring_min_plus.
+  Qed.
+
+  Definition mk_ring_map
+    : R₁ --> R₂.
+  Proof.
+    use mk_algebra_map.
+    - exact f.
+    - intros x.
+      induction x as [x | x].
+      + induction x as [x | x].
+        * induction x as [x | x].
+          ** induction x as [x | x].
+             *** apply f_p.
+             *** apply f_m.
+          ** exact (f_minus x).
+        * induction x.
+          exact f_z.
+      + induction x.
+        exact f_o.
+  Defined.
+End BuildRingMap.
+
+(** Laws of ring homomorphism *)
+Section RingMapProjections.
+  Context {R₁ R₂ : ring_cat}.
+  Variable (f : R₁ --> R₂).
+
+  Definition ring_map_plus
+    : ∏ (x y : alg_carrier R₁),
+      alg_map_carrier f (ring_plus R₁ x y)
+      =
+      ring_plus R₂ (alg_map_carrier f x) (alg_map_carrier f y)
+    := λ x y, eqtohomot (pr21 f) (inl (inl (inl (inl (x ,, y))))).
+
+  Definition ring_map_mult
+    : ∏ (x y : alg_carrier R₁),
+      alg_map_carrier f (ring_mult R₁ x y)
+      =
+      ring_mult R₂ (alg_map_carrier f x) (alg_map_carrier f y)
+    := λ x y, eqtohomot (pr21 f) (inl (inl (inl (inr (x ,, y))))).
+
+  Definition ring_map_minus
+    : ∏ (x : alg_carrier R₁),
+      alg_map_carrier f (ring_minus R₁ x)
+      =
+      ring_minus R₂ (alg_map_carrier f x)
+    := λ x, eqtohomot (pr21 f) (inl (inl (inr x))).
+
+  Definition ring_map_zero
+    : alg_map_carrier f (ring_zero R₁)
+      =
+      ring_zero R₂
+    := eqtohomot (pr21 f) (inl (inr tt)).
+
+  Definition ring_map_one
+    : alg_map_carrier f (ring_one R₁)
+      =
+      ring_one R₂
+    := eqtohomot (pr21 f) (inr tt).
+End RingMapProjections.
